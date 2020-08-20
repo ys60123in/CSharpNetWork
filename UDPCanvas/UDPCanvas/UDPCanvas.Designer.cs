7459b40f89f59e1d08a89a94554ef44ef4145f83
ys60123in <ys60123in@gmail.com>
2020-08-20 17:20:16 +0800

    新增UDP塗鴉牆專案

---
+++ UDPCanvas/UDPCanvas/UDPCanvas.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace UDPCanvas
+{
+    partial class UDPCanvas
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_TargetPort = new System.Windows.Forms.TextBox();
+            this.textBox_TargetIP = new System.Windows.Forms.TextBox();
+            this.textBox_ListenPort = new System.Windows.Forms.TextBox();
+            this.label_TargetPort = new System.Windows.Forms.Label();
+            this.label_TargetIP = new System.Windows.Forms.Label();
+            this.label_ListenPort = new System.Windows.Forms.Label();
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.radioButton_Red = new System.Windows.Forms.RadioButton();
+            this.radioButton_Green = new System.Windows.Forms.RadioButton();
+            this.radioButton_Blue = new System.Windows.Forms.RadioButton();
+            this.radioButton_Black = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox_TargetPort
+            // 
+            this.textBox_TargetPort.Location = new System.Drawing.Point(224, 12);
+            this.textBox_TargetPort.Name = "textBox_TargetPort";
+            this.textBox_TargetPort.Size = new System.Drawing.Size(100, 22);
+            this.textBox_TargetPort.TabIndex = 14;
+            // 
+            // textBox_TargetIP
+            // 
+            this.textBox_TargetIP.Location = new System.Drawing.Point(57, 12);
+            this.textBox_TargetIP.Name = "textBox_TargetIP";
+            this.textBox_TargetIP.Size = new System.Drawing.Size(100, 22);
+            this.textBox_TargetIP.TabIndex = 13;
+            // 
+            // textBox_ListenPort
+            // 
+            this.textBox_ListenPort.Location = new System.Drawing.Point(388, 12);
+            this.textBox_ListenPort.Name = "textBox_ListenPort";
+            this.textBox_ListenPort.Size = new System.Drawing.Size(100, 22);
+            this.textBox_ListenPort.TabIndex = 12;
+            // 
+            // label_TargetPort
+            // 
+            this.label_TargetPort.AutoSize = true;
+            this.label_TargetPort.Location = new System.Drawing.Point(170, 15);
+            this.label_TargetPort.Name = "label_TargetPort";
+            this.label_TargetPort.Size = new System.Drawing.Size(48, 12);
+            this.label_TargetPort.TabIndex = 11;
+            this.label_TargetPort.Text = "目標Port";
+            // 
+            // label_TargetIP
+            // 
+            this.label_TargetIP.AutoSize = true;
+            this.label_TargetIP.Location = new System.Drawing.Point(12, 15);
+            this.label_TargetIP.Name = "label_TargetIP";
+            this.label_TargetIP.Size = new System.Drawing.Size(39, 12);
+            this.label_TargetIP.TabIndex = 10;
+            this.label_TargetIP.Text = "目標IP";
+            // 
+            // label_ListenPort
+            // 
+            this.label_ListenPort.AutoSize = true;
+            this.label_ListenPort.Location = new System.Drawing.Point(334, 15);
+            this.label_ListenPort.Name = "label_ListenPort";
+            this.label_ListenPort.Size = new System.Drawing.Size(48, 12);
+            this.label_ListenPort.TabIndex = 9;
+            this.label_ListenPort.Text = "接聽Port";
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(505, 12);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(75, 23);
+            this.button_Connect.TabIndex = 15;
+            this.button_Connect.Text = "連線";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // radioButton_Red
+            // 
+            this.radioButton_Red.AutoSize = true;
+            this.radioButton_Red.Location = new System.Drawing.Point(14, 51);
+            this.radioButton_Red.Name = "radioButton_Red";
+            this.radioButton_Red.Size = new System.Drawing.Size(42, 16);
+            this.radioButton_Red.TabIndex = 16;
+            this.radioButton_Red.TabStop = true;
+            this.radioButton_Red.Text = "Red";
+            this.radioButton_Red.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_Green
+            // 
+            this.radioButton_Green.AutoSize = true;
+            this.radioButton_Green.Location = new System.Drawing.Point(62, 51);
+            this.radioButton_Green.Name = "radioButton_Green";
+            this.radioButton_Green.Size = new System.Drawing.Size(51, 16);
+            this.radioButton_Green.TabIndex = 17;
+            this.radioButton_Green.TabStop = true;
+            this.radioButton_Green.Text = "Green";
+            this.radioButton_Green.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_Blue
+            // 
+            this.radioButton_Blue.AutoSize = true;
+            this.radioButton_Blue.Location = new System.Drawing.Point(119, 51);
+            this.radioButton_Blue.Name = "radioButton_Blue";
+            this.radioButton_Blue.Size = new System.Drawing.Size(45, 16);
+            this.radioButton_Blue.TabIndex = 18;
+            this.radioButton_Blue.TabStop = true;
+            this.radioButton_Blue.Text = "Blue";
+            this.radioButton_Blue.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_Black
+            // 
+            this.radioButton_Black.AutoSize = true;
+            this.radioButton_Black.Location = new System.Drawing.Point(172, 51);
+            this.radioButton_Black.Name = "radioButton_Black";
+            this.radioButton_Black.Size = new System.Drawing.Size(50, 16);
+            this.radioButton_Black.TabIndex = 19;
+            this.radioButton_Black.TabStop = true;
+            this.radioButton_Black.Text = "Black";
+            this.radioButton_Black.UseVisualStyleBackColor = true;
+            // 
+            // UDPCanvas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 450);
+            this.Controls.Add(this.radioButton_Black);
+            this.Controls.Add(this.radioButton_Blue);
+            this.Controls.Add(this.radioButton_Green);
+            this.Controls.Add(this.radioButton_Red);
+            this.Controls.Add(this.button_Connect);
+            this.Controls.Add(this.textBox_TargetPort);
+            this.Controls.Add(this.textBox_TargetIP);
+            this.Controls.Add(this.textBox_ListenPort);
+            this.Controls.Add(this.label_TargetPort);
+            this.Controls.Add(this.label_TargetIP);
+            this.Controls.Add(this.label_ListenPort);
+            this.Name = "UDPCanvas";
+            this.Text = "UDP塗鴉牆";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UDPCanvas_FormClosing);
+            this.Load += new System.EventHandler(this.UDPCanvas_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.UDPCanvas_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.UDPCanvas_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UDPCanvas_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_TargetPort;
+        private System.Windows.Forms.TextBox textBox_TargetIP;
+        private System.Windows.Forms.TextBox textBox_ListenPort;
+        private System.Windows.Forms.Label label_TargetPort;
+        private System.Windows.Forms.Label label_TargetIP;
+        private System.Windows.Forms.Label label_ListenPort;
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.RadioButton radioButton_Red;
+        private System.Windows.Forms.RadioButton radioButton_Green;
+        private System.Windows.Forms.RadioButton radioButton_Blue;
+        private System.Windows.Forms.RadioButton radioButton_Black;
+    }
+}
+
---
+++ UDPCanvas/UDPCanvas/UDPCanvas.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.VisualBasic.PowerPacks;
+
+namespace UDPCanvas
+{
+    public partial class UDPCanvas : Form
+    {
+        UdpClient U; //宣告UDP通訊物件
+        Thread Th; //宣告監聽用執行緒
+        //繪圖相關變數宣告
+        ShapeContainer C; //畫布物件(本機繪圖用)
+        ShapeContainer D; //畫布物件(遠端繪圖用)
+        Point stP; //繪圖起點
+        string p; //筆畫座標字串
+
+        public UDPCanvas()
+        {
+            InitializeComponent();
+        }
+
+        //表單載入
+        private void UDPCanvas_Load(object sender, EventArgs e)
+        {
+            C = new ShapeContainer(); //本機繪圖用
+            this.Controls.Add(C); //加入畫布C到表單
+            D = new ShapeContainer(); //遠端繪圖用
+            this.Controls.Add(D); //加入畫布D到表單
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false; //忽略跨執行緒錯誤
+            Th = new Thread(Listen); //建立監聽執行緒，目標副程序->Listen
+            Th.Start(); //啟動監聽執行緒
+            button_Connect.Enabled = false; //使按鍵失效，不能(也不需要)重複開啟監聽
+        }
+
+        //監聽副程序
+        private void Listen()
+        {
+            int Port = int.Parse(textBox_ListenPort.Text); //設定監聽用的通訊阜
+            U = new UdpClient(Port); //監聽UDP監聽器實體
+            //建立本機端點資訊
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port);
+            while (true) //持續監聽的無限迴圈
+            {
+                byte[] B = U.Receive(ref EP); //訊息傳達時讀取資訊到B陣列
+                string A = Encoding.Default.GetString(B); //翻譯B陣列為字串A
+                string[] Z = A.Split('_'); //切割顏色與座標資訊
+                string[] Q = Z[1].Split('/'); //切割座標資訊
+                Point[] R = new Point[Q.Length]; //宣告座標點陣列
+                for(int i = 0; i < Q.Length; i++) 
+                {
+                    string[] K = Q[i].Split(','); //切割X與Y座標
+                    R[i].X = int.Parse(K[0]); //定義第i點X座標
+                    R[i].Y = int.Parse(K[1]); //定義第i點Y座標
+                }
+                for (int i = 0; i < Q.Length - 1; i++)
+                {
+                    LineShape L = new LineShape(); //建立線段物件
+                    L.StartPoint = R[i]; //線段起點
+                    L.EndPoint = R[i + 1]; //線段終點
+                    switch (Z[0]) //筆色
+                    {
+                        case "1": //紅筆
+                            L.BorderColor = Color.Red;
+                            break;
+                        case "2": //亮綠色筆
+                            L.BorderColor = Color.Lime;
+                            break;
+                        case "3": //藍筆
+                            L.BorderColor = Color.Blue;
+                            break;
+                        case "4": //黑筆
+                            L.BorderColor = Color.Black;
+                            break;
+                    }
+                    L.Parent = D; //線段L加入畫布D(遠端使用者繪圖)
+                }
+            }
+        }
+
+        private void UDPCanvas_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Th.Abort(); //關閉監聽執行緒
+                U.Close(); //關閉監聽器
+            } 
+            catch (Exception)
+            {
+                //忽略錯誤
+            }
+        }
+
+        //本機端開始繪圖
+        private void UDPCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            stP = e.Location; //起點
+            p = stP.X.ToString() + "," + stP.Y.ToString(); //起點座標記錄
+        }
+
+        //本機繪圖中
+        private void UDPCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                LineShape L = new LineShape(); //建立線段物件
+                L.StartPoint = stP; //線段起點
+                L.EndPoint = e.Location; //線段終點
+                if(radioButton_Red.Checked) { L.BorderColor = Color.Red; } //紅筆
+                if (radioButton_Green.Checked) { L.BorderColor = Color.Lime; } //亮綠色筆
+                if (radioButton_Blue.Checked) { L.BorderColor = Color.Blue; } //藍筆
+                if (radioButton_Black.Checked) { L.BorderColor = Color.Black; } //黑筆
+                L.Parent = C; //線段加入畫布C
+                stP = e.Location; //終點變起點
+                p += "/" + stP.X.ToString() + "," + stP.Y.ToString(); //持續記錄座標
+            }
+        }
+
+        //送出繪圖動作
+        private void UDPCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            string IP = textBox_TargetIP.Text; //設定發送的目標IP
+            int Port = int.Parse(textBox_TargetPort.Text); //設定發送的目標Port
+            UdpClient S = new UdpClient(IP, Port); //建立UDP物件
+            if (radioButton_Red.Checked) { p = "1_" + p; } //紅筆
+            if (radioButton_Green.Checked) { p = "2_" + p; } //亮綠色筆
+            if (radioButton_Blue.Checked) { p = "3_" + p; } //藍筆
+            if (radioButton_Black.Checked) { p = "4_" + p; } //黑筆
+            byte[] B = Encoding.Default.GetBytes(p); //翻譯p字串為B陣列
+            S.Send(B, B.Length); //發送資料
+            S.Close(); //關閉UDP物件
+        }
+    }
+}
