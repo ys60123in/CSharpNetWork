fa8c1e44deacd24262ebd9c00ee66d47cccfe8fb
ys60123in <ys60123in@gmail.com>
2020-08-20 20:12:02 +0800

    新增UDP聊天室程式

---
+++ UDPChatRoom/UDPChatRoom/UDPChatRoom.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace UDPChatRoom
+{
+    partial class UDPChatRoom
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_OnlineUser = new System.Windows.Forms.Label();
+            this.label_Msg = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox_OnlineUser = new System.Windows.Forms.ListBox();
+            this.listBox_Msg = new System.Windows.Forms.ListBox();
+            this.textBox_User = new System.Windows.Forms.TextBox();
+            this.textBox_Msg = new System.Windows.Forms.TextBox();
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.button_Broadcast = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_OnlineUser
+            // 
+            this.label_OnlineUser.AutoSize = true;
+            this.label_OnlineUser.Location = new System.Drawing.Point(46, 13);
+            this.label_OnlineUser.Name = "label_OnlineUser";
+            this.label_OnlineUser.Size = new System.Drawing.Size(65, 12);
+            this.label_OnlineUser.TabIndex = 0;
+            this.label_OnlineUser.Text = "線上使用者";
+            // 
+            // label_Msg
+            // 
+            this.label_Msg.AutoSize = true;
+            this.label_Msg.Location = new System.Drawing.Point(370, 13);
+            this.label_Msg.Name = "label_Msg";
+            this.label_Msg.Size = new System.Drawing.Size(53, 12);
+            this.label_Msg.TabIndex = 1;
+            this.label_Msg.Text = "訊息視窗";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "我是：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(159, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "訊息：";
+            // 
+            // listBox_OnlineUser
+            // 
+            this.listBox_OnlineUser.FormattingEnabled = true;
+            this.listBox_OnlineUser.ItemHeight = 12;
+            this.listBox_OnlineUser.Location = new System.Drawing.Point(12, 33);
+            this.listBox_OnlineUser.Name = "listBox_OnlineUser";
+            this.listBox_OnlineUser.Size = new System.Drawing.Size(141, 328);
+            this.listBox_OnlineUser.TabIndex = 4;
+            // 
+            // listBox_Msg
+            // 
+            this.listBox_Msg.FormattingEnabled = true;
+            this.listBox_Msg.ItemHeight = 12;
+            this.listBox_Msg.Location = new System.Drawing.Point(159, 33);
+            this.listBox_Msg.Name = "listBox_Msg";
+            this.listBox_Msg.Size = new System.Drawing.Size(470, 232);
+            this.listBox_Msg.TabIndex = 5;
+            // 
+            // textBox_User
+            // 
+            this.textBox_User.Location = new System.Drawing.Point(206, 282);
+            this.textBox_User.Name = "textBox_User";
+            this.textBox_User.Size = new System.Drawing.Size(342, 22);
+            this.textBox_User.TabIndex = 6;
+            // 
+            // textBox_Msg
+            // 
+            this.textBox_Msg.Location = new System.Drawing.Point(206, 312);
+            this.textBox_Msg.Name = "textBox_Msg";
+            this.textBox_Msg.Size = new System.Drawing.Size(423, 22);
+            this.textBox_Msg.TabIndex = 7;
+            this.textBox_Msg.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_Msg_KeyDown);
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(554, 281);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(75, 23);
+            this.button_Connect.TabIndex = 8;
+            this.button_Connect.Text = "上線";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // button_Broadcast
+            // 
+            this.button_Broadcast.Location = new System.Drawing.Point(554, 340);
+            this.button_Broadcast.Name = "button_Broadcast";
+            this.button_Broadcast.Size = new System.Drawing.Size(75, 23);
+            this.button_Broadcast.TabIndex = 9;
+            this.button_Broadcast.Text = "廣播";
+            this.button_Broadcast.UseVisualStyleBackColor = true;
+            this.button_Broadcast.Click += new System.EventHandler(this.button_Broadcast_Click);
+            // 
+            // UDPChatRoom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 369);
+            this.Controls.Add(this.button_Broadcast);
+            this.Controls.Add(this.button_Connect);
+            this.Controls.Add(this.textBox_Msg);
+            this.Controls.Add(this.textBox_User);
+            this.Controls.Add(this.listBox_Msg);
+            this.Controls.Add(this.listBox_OnlineUser);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label_Msg);
+            this.Controls.Add(this.label_OnlineUser);
+            this.Name = "UDPChatRoom";
+            this.Text = "UDP聊天室";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UDPChatRoom_FormClosing);
+            this.Load += new System.EventHandler(this.UDPChatRoom_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_OnlineUser;
+        private System.Windows.Forms.Label label_Msg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox_OnlineUser;
+        private System.Windows.Forms.ListBox listBox_Msg;
+        private System.Windows.Forms.TextBox textBox_User;
+        private System.Windows.Forms.TextBox textBox_Msg;
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.Button button_Broadcast;
+    }
+}
+
---
+++ UDPChatRoom/UDPChatRoom/UDPChatRoom.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections; //匯入集合物件功能
+
+namespace UDPChatRoom
+{
+    public partial class UDPChatRoom : Form
+    {
+        UdpClient U; //宣告UDP通訊物件
+        Thread Th; //宣告監聽用執行緒
+        String MyName; //我的名稱
+        ArrayList ips = new ArrayList(); //線上客戶IP列表
+        const short Port = 2019; //本程式使用的通訊埠(頻道)
+        string BC = IPAddress.Broadcast.ToString(); //廣播用IP
+
+        public UDPChatRoom()
+        {
+            InitializeComponent();
+        }
+
+        private void UDPChatRoom_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false; //忽略跨執行續操作的錯誤
+            this.Text += " " + MyIP(); //顯示本機IP於標題列
+        }
+
+        //關閉監聽執行緒(如果有的話)
+        private void UDPChatRoom_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Th.Abort(); //關閉監聽執行緒
+                U.Close(); //關閉監聽器
+            }
+            catch (Exception)
+            {
+                //忽略錯誤
+            }
+        }
+
+        //上線或離線的選擇
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(textBox_User.Text))
+            {
+                MessageBox.Show("請輸入姓名!");
+                return;
+            }
+
+            MyName = textBox_User.Text; //我的名稱
+            listBox_OnlineUser.Items.Clear(); //清除名單
+            ips.Clear(); //清除線上人員IP陣列
+            
+            if(button_Connect.Text.Equals("上線"))
+            {
+                Th = new Thread(Listen); //建立監聽網路訊息的新執行緒
+                Th.Start(); //啟動監聽執行緒
+                Send(BC, "OnLine", ""); //公告上線訊息
+                button_Connect.Text = "離線";
+            }
+            else
+            {
+                Send(BC, "OffLine", ""); //公告離線訊息
+                Th.Abort(); //關閉監聽執行緒
+                U.Close(); //關閉監聽器
+                button_Connect.Text = "上線";
+            }
+        }
+
+        //廣播
+        private void button_Broadcast_Click(object sender, EventArgs e)
+        {
+            listBox_OnlineUser.ClearSelected();
+        }
+
+        //找出本機IP
+        private string MyIP()
+        {
+            string hn = Dns.GetHostName(); //取得本機電腦名稱
+            IPAddress[] ip = Dns.GetHostEntry(hn).AddressList; //取得本機IP陣列(可能有多個)
+            foreach (IPAddress it in ip)
+            {
+                if (it.AddressFamily == AddressFamily.InterNetwork) //如果是IPv4格式
+                {
+                    return it.ToString(); //回傳此IP字串
+                }
+            }
+
+            return ""; //找不到合格IP，回傳空字串
+        }
+
+        private void Send(string ToIP, string msg, string toWhom)
+        {
+            //訊息格式:我是誰+IP+訊息+發給誰
+            string A = string.Format("{0}:{1}:{2}:{3}", MyName, MyIP(), msg, toWhom);
+            Byte[] B = Encoding.Default.GetBytes(A); //字串翻譯成位元組列
+            UdpClient V = new UdpClient(ToIP, Port); //建立UDP通訊物件
+            V.Send(B, B.Length); //發送資料
+        }
+
+        //監聽副程序
+        private void Listen()
+        {
+            U = new UdpClient(Port); //建立UDP通訊物件
+            //接聽端點(IP+Port)
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(MyIP()), Port);
+            while (true)
+            {
+                byte[] B = U.Receive(ref EP); //訊息到達時讀取信息到B陣列
+                string A = Encoding.Default.GetString(B); //翻譯B陣列為字串A
+                //切割訊息為:C[0] = 發訊者;C[1] = IP;C[2] = 訊息;C[3] = 發訊對象
+                string[] C = A.Split(':');
+                switch (C[2]) //根據訊息內容做動作
+                {
+                    case "OnLine":
+                        listBox_OnlineUser.Items.Add(C[0]); //名稱加入列表
+                        ips.Add(C[1]); //IP加入集合物件
+                        if (C[0] != MyName) 
+                        { 
+                            Send(C[1], "AddMe", C[0]); //回應我也在線上 
+                        }
+                        break;
+                    case "AddMe":
+                        listBox_OnlineUser.Items.Add(C[0]); //名稱加入列表
+                        ips.Add(C[1]); //IP加入集合物件
+                        break;
+                    case "OffLine":
+                        listBox_OnlineUser.Items.Remove(C[0]); //移除名單
+                        ips.Remove(C[1]); //移除IP
+                        break;
+                    default:
+                        if (C[3] == "") //公開訊息(無指定收訊者)
+                        {
+                            listBox_Msg.Items.Add(C[0] + "(廣播):" + C[2]); //加入看板
+                        }
+                        else //私密訊息(有指定收訊者C[3])
+                        {
+                            listBox_Msg.Items.Add(C[0] + " to " + C[3] + " : " + C[2]); //加入看板
+                        }
+                        break;
+                }
+            }
+        }
+
+        //發送自訂訊息
+        private void textBox_Msg_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if(listBox_OnlineUser.SelectedIndex < 0) //未選擇發訊對象
+                {
+                    Send(BC, textBox_Msg.Text, "");
+                }
+                else //發送私密訊息
+                {
+                    Send(ips[listBox_OnlineUser.SelectedIndex].ToString(), textBox_Msg.Text, listBox_OnlineUser.SelectedItem.ToString());
+                    listBox_Msg.Items.Add(MyName + " to " + listBox_OnlineUser.SelectedItem.ToString() + " : " + textBox_Msg.Text); //訊息寫入看板
+                }
+                textBox_Msg.Text = "";
+            }
+        }
+    }
+}
