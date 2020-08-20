656035617f404d87e630ac636ba03e5f3c8c77de
ys60123in <ys60123in@gmail.com>
2020-08-20 21:38:06 +0800

    新增UDPClient程式

---
+++ UDPClient/UDPClient/UDPClient.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace UDPClient
+{
+    partial class UDPClient
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Command = new System.Windows.Forms.TextBox();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.button_Command = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "詢問：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "接收：";
+            // 
+            // textBox_Command
+            // 
+            this.textBox_Command.Location = new System.Drawing.Point(59, 18);
+            this.textBox_Command.Name = "textBox_Command";
+            this.textBox_Command.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Command.TabIndex = 2;
+            // 
+            // textBox_Result
+            // 
+            this.textBox_Result.Location = new System.Drawing.Point(59, 64);
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.Size = new System.Drawing.Size(215, 22);
+            this.textBox_Result.TabIndex = 3;
+            // 
+            // button_Command
+            // 
+            this.button_Command.Location = new System.Drawing.Point(180, 16);
+            this.button_Command.Name = "button_Command";
+            this.button_Command.Size = new System.Drawing.Size(75, 23);
+            this.button_Command.TabIndex = 4;
+            this.button_Command.Text = "送出";
+            this.button_Command.UseVisualStyleBackColor = true;
+            this.button_Command.Click += new System.EventHandler(this.button_Command_Click);
+            // 
+            // UDPClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 107);
+            this.Controls.Add(this.button_Command);
+            this.Controls.Add(this.textBox_Result);
+            this.Controls.Add(this.textBox_Command);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "UDPClient";
+            this.Text = "UDPClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Command;
+        private System.Windows.Forms.TextBox textBox_Result;
+        private System.Windows.Forms.Button button_Command;
+    }
+}
+
---
+++ UDPClient/UDPClient/UDPClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Xml.Xsl;
+
+namespace UDPClient
+{
+    public partial class UDPClient : Form
+    {
+        const int Port = 2019;
+
+        public UDPClient()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Command_Click(object sender, EventArgs e)
+        {
+            UdpClient C = new UdpClient();
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port); //應為伺服器端所在IP
+            C.Connect(EP);
+            byte[] B = Encoding.Default.GetBytes(textBox_Command.Text); //送出問題
+            C.Send(B, B.Length);
+            byte[] R = C.Receive(ref EP); //原路接收訊息
+            textBox_Result.Text = Encoding.Default.GetString(R);
+        }
+    }
+}
