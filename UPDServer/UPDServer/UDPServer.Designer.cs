0ea144895ed5b022007ee8191d9d1b0eaef494b9
ys60123in <ys60123in@gmail.com>
2020-08-20 21:25:52 +0800

    建立UDPServer程式

---
+++ UPDServer/UPDServer/UDPServer.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace UPDServer
+{
+    partial class UDPServer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UDPServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 174);
+            this.Name = "UDPServer";
+            this.Text = "UDPServer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UDPServer_FormClosing);
+            this.Load += new System.EventHandler(this.UDPServer_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ UPDServer/UPDServer/UDPServer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace UPDServer
+{
+    public partial class UDPServer : Form
+    {
+        Thread Th;
+        const int Port = 2019;
+
+        public UDPServer()
+        {
+            InitializeComponent();
+        }
+
+        private void UDPServer_Load(object sender, EventArgs e)
+        {
+            Th = new Thread(Listen); //建立監聽執行緒
+            Th.IsBackground = true; //設為背景執行緒
+            Th.Start();
+        }
+
+        private void UDPServer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Th.Abort(); //關閉執行緒
+        }
+
+        //伺服器監聽程式
+        private void Listen() 
+        {
+            UdpClient U = new UdpClient(Port);
+            while(true)
+            {
+                IPEndPoint EP = new IPEndPoint(IPAddress.Any, Port); //建立監聽端點資訊(接收任何IP)
+                byte[] B = U.Receive(ref EP);
+                string A = Encoding.Default.GetString(B);
+                string M = "Unknown Command";
+                if (A == "Time?")
+                {
+                    M = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+                }
+                B = Encoding.Default.GetBytes(M);
+                U.Send(B, B.Length, EP);
+            }
+        }
+    }
+}
