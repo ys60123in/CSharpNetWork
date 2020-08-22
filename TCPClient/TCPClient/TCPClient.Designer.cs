54580543f278fd620ced96935149b482fd6deebe
ys60123in <ys60123in@gmail.com>
2020-08-22 14:54:46 +0800

    新增TCP Client程式

---
+++ TCPClient/TCPClient/TCPClient.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace TCPClient
+{
+    partial class TCPClient
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Login = new System.Windows.Forms.Button();
+            this.label_ServerIP = new System.Windows.Forms.Label();
+            this.textBox_ServerIP = new System.Windows.Forms.TextBox();
+            this.label_UserName = new System.Windows.Forms.Label();
+            this.label_ServerPort = new System.Windows.Forms.Label();
+            this.textBox_UserName = new System.Windows.Forms.TextBox();
+            this.textBox_ServerPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Login
+            // 
+            this.button_Login.Location = new System.Drawing.Point(273, 61);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(75, 23);
+            this.button_Login.TabIndex = 0;
+            this.button_Login.Text = "登入伺服器";
+            this.button_Login.UseVisualStyleBackColor = true;
+            this.button_Login.Click += new System.EventHandler(this.button_Login_Click);
+            // 
+            // label_ServerIP
+            // 
+            this.label_ServerIP.AutoSize = true;
+            this.label_ServerIP.Location = new System.Drawing.Point(8, 23);
+            this.label_ServerIP.Name = "label_ServerIP";
+            this.label_ServerIP.Size = new System.Drawing.Size(51, 12);
+            this.label_ServerIP.TabIndex = 1;
+            this.label_ServerIP.Text = "伺服器IP";
+            // 
+            // textBox_ServerIP
+            // 
+            this.textBox_ServerIP.Location = new System.Drawing.Point(65, 20);
+            this.textBox_ServerIP.Name = "textBox_ServerIP";
+            this.textBox_ServerIP.Size = new System.Drawing.Size(100, 22);
+            this.textBox_ServerIP.TabIndex = 2;
+            // 
+            // label_UserName
+            // 
+            this.label_UserName.AutoSize = true;
+            this.label_UserName.Location = new System.Drawing.Point(6, 64);
+            this.label_UserName.Name = "label_UserName";
+            this.label_UserName.Size = new System.Drawing.Size(53, 12);
+            this.label_UserName.TabIndex = 3;
+            this.label_UserName.Text = "玩家名稱";
+            // 
+            // label_ServerPort
+            // 
+            this.label_ServerPort.AutoSize = true;
+            this.label_ServerPort.Location = new System.Drawing.Point(182, 23);
+            this.label_ServerPort.Name = "label_ServerPort";
+            this.label_ServerPort.Size = new System.Drawing.Size(60, 12);
+            this.label_ServerPort.TabIndex = 4;
+            this.label_ServerPort.Text = "伺服器Port";
+            // 
+            // textBox_UserName
+            // 
+            this.textBox_UserName.Location = new System.Drawing.Point(65, 61);
+            this.textBox_UserName.Name = "textBox_UserName";
+            this.textBox_UserName.Size = new System.Drawing.Size(100, 22);
+            this.textBox_UserName.TabIndex = 5;
+            // 
+            // textBox_ServerPort
+            // 
+            this.textBox_ServerPort.Location = new System.Drawing.Point(248, 20);
+            this.textBox_ServerPort.Name = "textBox_ServerPort";
+            this.textBox_ServerPort.Size = new System.Drawing.Size(100, 22);
+            this.textBox_ServerPort.TabIndex = 6;
+            // 
+            // TCPClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 97);
+            this.Controls.Add(this.textBox_ServerPort);
+            this.Controls.Add(this.textBox_UserName);
+            this.Controls.Add(this.label_ServerPort);
+            this.Controls.Add(this.label_UserName);
+            this.Controls.Add(this.textBox_ServerIP);
+            this.Controls.Add(this.label_ServerIP);
+            this.Controls.Add(this.button_Login);
+            this.Name = "TCPClient";
+            this.Text = "TCP Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TCPClient_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.Label label_ServerIP;
+        private System.Windows.Forms.TextBox textBox_ServerIP;
+        private System.Windows.Forms.Label label_UserName;
+        private System.Windows.Forms.Label label_ServerPort;
+        private System.Windows.Forms.TextBox textBox_UserName;
+        private System.Windows.Forms.TextBox textBox_ServerPort;
+    }
+}
+
---
+++ TCPClient/TCPClient/TCPClient.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPClient
+{
+    public partial class TCPClient : Form
+    {
+        Socket T; //通訊物件
+        string User; //使用者
+
+        public TCPClient()
+        {
+            InitializeComponent();
+        }
+
+        //關閉視窗代表離線登出
+        private void TCPClient_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(button_Login.Enabled == false)
+            {
+                Send("9" + User); //傳送自己離線的訊息給伺服器
+                T.Close(); //關閉網路通訊器T
+            }
+        }
+
+        //登入伺服器
+        private void button_Login_Click(object sender, EventArgs e)
+        {
+            string IP = textBox_ServerIP.Text; //伺服器IP
+            int Port = int.Parse(textBox_ServerPort.Text); //伺服器Port
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(IP), Port); //伺服器的連線端點資訊
+
+            //建立可以雙向通訊的TCP連線
+            T = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            User = textBox_UserName.Text; //使用者名稱
+
+            try
+            {
+                T.Connect(EP); //連上伺服器的端點EP(類似撥號給電話總機)
+                Send("0" + User);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("無法連上伺服器! "); //連線失敗時顯示訊息
+                return;
+            }
+            button_Login.Enabled = false; //讓連線按鍵失敗，避免重複連線
+        }
+
+        //傳送訊息給Server
+        private void Send(String Str)
+        {
+            byte[] B = Encoding.Default.GetBytes(Str);
+            T.Send(B, 0, B.Length, SocketFlags.None); //使用連線物件傳送資料
+        }
+    }
+}
