652037feec2729d5977b8284c83adb78ffe1df34
ys60123in <ys60123in@gmail.com>
2020-08-22 20:38:41 +0800

    新增TCP ChatRoom Client程式

---
+++ TCPChatRoomClient/TCPClient/TCPChatRoomClient.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace TCPChatRoomClient
+{
+    partial class TCPChatRoomClient
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Login = new System.Windows.Forms.Button();
+            this.label_ServerIP = new System.Windows.Forms.Label();
+            this.textBox_ServerIP = new System.Windows.Forms.TextBox();
+            this.label_UserName = new System.Windows.Forms.Label();
+            this.label_ServerPort = new System.Windows.Forms.Label();
+            this.textBox_UserName = new System.Windows.Forms.TextBox();
+            this.textBox_ServerPort = new System.Windows.Forms.TextBox();
+            this.button_Broadcast = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Messages = new System.Windows.Forms.TextBox();
+            this.listBox_User = new System.Windows.Forms.ListBox();
+            this.button_Enter = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Msg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Login
+            // 
+            this.button_Login.Location = new System.Drawing.Point(273, 61);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(75, 23);
+            this.button_Login.TabIndex = 0;
+            this.button_Login.Text = "登入伺服器";
+            this.button_Login.UseVisualStyleBackColor = true;
+            this.button_Login.Click += new System.EventHandler(this.button_Login_Click);
+            // 
+            // label_ServerIP
+            // 
+            this.label_ServerIP.AutoSize = true;
+            this.label_ServerIP.Location = new System.Drawing.Point(8, 23);
+            this.label_ServerIP.Name = "label_ServerIP";
+            this.label_ServerIP.Size = new System.Drawing.Size(51, 12);
+            this.label_ServerIP.TabIndex = 1;
+            this.label_ServerIP.Text = "伺服器IP";
+            // 
+            // textBox_ServerIP
+            // 
+            this.textBox_ServerIP.Location = new System.Drawing.Point(65, 20);
+            this.textBox_ServerIP.Name = "textBox_ServerIP";
+            this.textBox_ServerIP.Size = new System.Drawing.Size(100, 22);
+            this.textBox_ServerIP.TabIndex = 2;
+            // 
+            // label_UserName
+            // 
+            this.label_UserName.AutoSize = true;
+            this.label_UserName.Location = new System.Drawing.Point(6, 64);
+            this.label_UserName.Name = "label_UserName";
+            this.label_UserName.Size = new System.Drawing.Size(53, 12);
+            this.label_UserName.TabIndex = 3;
+            this.label_UserName.Text = "玩家名稱";
+            // 
+            // label_ServerPort
+            // 
+            this.label_ServerPort.AutoSize = true;
+            this.label_ServerPort.Location = new System.Drawing.Point(182, 23);
+            this.label_ServerPort.Name = "label_ServerPort";
+            this.label_ServerPort.Size = new System.Drawing.Size(60, 12);
+            this.label_ServerPort.TabIndex = 4;
+            this.label_ServerPort.Text = "伺服器Port";
+            // 
+            // textBox_UserName
+            // 
+            this.textBox_UserName.Location = new System.Drawing.Point(65, 61);
+            this.textBox_UserName.Name = "textBox_UserName";
+            this.textBox_UserName.Size = new System.Drawing.Size(100, 22);
+            this.textBox_UserName.TabIndex = 5;
+            // 
+            // textBox_ServerPort
+            // 
+            this.textBox_ServerPort.Location = new System.Drawing.Point(248, 20);
+            this.textBox_ServerPort.Name = "textBox_ServerPort";
+            this.textBox_ServerPort.Size = new System.Drawing.Size(100, 22);
+            this.textBox_ServerPort.TabIndex = 6;
+            // 
+            // button_Broadcast
+            // 
+            this.button_Broadcast.Location = new System.Drawing.Point(354, 19);
+            this.button_Broadcast.Name = "button_Broadcast";
+            this.button_Broadcast.Size = new System.Drawing.Size(84, 23);
+            this.button_Broadcast.TabIndex = 7;
+            this.button_Broadcast.Text = "廣播";
+            this.button_Broadcast.UseVisualStyleBackColor = true;
+            this.button_Broadcast.Click += new System.EventHandler(this.button_Broadcast_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(365, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "線上使用者";
+            // 
+            // textBox_Messages
+            // 
+            this.textBox_Messages.Location = new System.Drawing.Point(8, 95);
+            this.textBox_Messages.Multiline = true;
+            this.textBox_Messages.Name = "textBox_Messages";
+            this.textBox_Messages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Messages.Size = new System.Drawing.Size(340, 208);
+            this.textBox_Messages.TabIndex = 9;
+            // 
+            // listBox_User
+            // 
+            this.listBox_User.FormattingEnabled = true;
+            this.listBox_User.ItemHeight = 12;
+            this.listBox_User.Location = new System.Drawing.Point(354, 95);
+            this.listBox_User.Name = "listBox_User";
+            this.listBox_User.Size = new System.Drawing.Size(84, 208);
+            this.listBox_User.TabIndex = 10;
+            // 
+            // button_Enter
+            // 
+            this.button_Enter.Enabled = false;
+            this.button_Enter.Location = new System.Drawing.Point(354, 309);
+            this.button_Enter.Name = "button_Enter";
+            this.button_Enter.Size = new System.Drawing.Size(84, 23);
+            this.button_Enter.TabIndex = 11;
+            this.button_Enter.Text = "送出";
+            this.button_Enter.UseVisualStyleBackColor = true;
+            this.button_Enter.Click += new System.EventHandler(this.button_Enter_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "發言內容";
+            // 
+            // textBox_Msg
+            // 
+            this.textBox_Msg.Location = new System.Drawing.Point(65, 309);
+            this.textBox_Msg.Name = "textBox_Msg";
+            this.textBox_Msg.Size = new System.Drawing.Size(283, 22);
+            this.textBox_Msg.TabIndex = 13;
+            // 
+            // TCPChatRoomClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 339);
+            this.Controls.Add(this.textBox_Msg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button_Enter);
+            this.Controls.Add(this.listBox_User);
+            this.Controls.Add(this.textBox_Messages);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Broadcast);
+            this.Controls.Add(this.textBox_ServerPort);
+            this.Controls.Add(this.textBox_UserName);
+            this.Controls.Add(this.label_ServerPort);
+            this.Controls.Add(this.label_UserName);
+            this.Controls.Add(this.textBox_ServerIP);
+            this.Controls.Add(this.label_ServerIP);
+            this.Controls.Add(this.button_Login);
+            this.Name = "TCPChatRoomClient";
+            this.Text = "TCP Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TCPClient_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.Label label_ServerIP;
+        private System.Windows.Forms.TextBox textBox_ServerIP;
+        private System.Windows.Forms.Label label_UserName;
+        private System.Windows.Forms.Label label_ServerPort;
+        private System.Windows.Forms.TextBox textBox_UserName;
+        private System.Windows.Forms.TextBox textBox_ServerPort;
+        private System.Windows.Forms.Button button_Broadcast;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Messages;
+        private System.Windows.Forms.ListBox listBox_User;
+        private System.Windows.Forms.Button button_Enter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Msg;
+    }
+}
+
---
+++ TCPChatRoomClient/TCPClient/TCPChatRoomClient.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCPChatRoomClient
+{
+    public partial class TCPChatRoomClient : Form
+    {
+        Socket T; //通訊物件
+        Thread Th; //網路監聽執行緒
+        string User; //使用者
+
+        public TCPChatRoomClient()
+        {
+            InitializeComponent();
+        }
+
+        //關閉視窗代表離線登出
+        private void TCPClient_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(button_Login.Enabled == false)
+            {
+                Send("9" + User); //傳送自己離線的訊息給伺服器
+                T.Close(); //關閉網路通訊器T
+            }
+        }
+
+        //登入伺服器
+        private void button_Login_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false; //忽略跨執行緒錯誤
+            string IP = textBox_ServerIP.Text; //伺服器IP
+            int Port = int.Parse(textBox_ServerPort.Text); //伺服器Port
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(IP), Port); //伺服器的連線端點資訊
+
+            //建立可以雙向通訊的TCP連線
+            T = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            User = textBox_UserName.Text; //使用者名稱
+
+            try
+            {
+                T.Connect(EP); //連上伺服器的端點EP(類似撥號給電話總機)
+                Th = new Thread(Listen); //建立監聽執行緒
+                Th.IsBackground = true; //設為背景執行緒
+                Th.Start(); //開始監聽
+                textBox_Messages.Text = "已連線伺服器! " + "\r\n";
+                Send("0" + User);
+            }
+            catch (Exception)
+            {
+                textBox_Messages.Text = "無法連上伺服器! " + "\r\n"; //連線失敗時顯示訊息
+                return;
+            }
+            button_Login.Enabled = false; //讓連線按鍵失敗，避免重複連線
+            button_Enter.Enabled = true; //如果連線成功則可以開始發送訊息
+        }
+
+        //廣播
+        private void button_Broadcast_Click(object sender, EventArgs e)
+        {
+            listBox_User.ClearSelected(); //清除選取
+        }
+
+        //送出
+        private void button_Enter_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox_Msg.Text)) return; //未輸入訊息不得傳送資料
+            if(listBox_User.SelectedIndex < 0) //未選取傳送對象(廣播)，命令碼:1
+            {
+                Send("1" + User + " 公告:" + textBox_Msg.Text);
+            }
+            else
+            {
+                Send("2" + "來自" + User + ":" + textBox_Msg.Text + "|" + listBox_User.SelectedItem);
+                textBox_Messages.Text += "告訴" + listBox_User.SelectedItem + ":" + textBox_Msg.Text + "\r\n";
+            }
+            textBox_Msg.Text = ""; //清除發言框
+        }
+
+        //傳送訊息給Server
+        private void Send(String Str)
+        {
+            byte[] B = Encoding.Default.GetBytes(Str);
+            T.Send(B, 0, B.Length, SocketFlags.None); //使用連線物件傳送資料
+        }
+
+        //監聽Server信息
+        private void Listen()
+        {
+            EndPoint ServerEP = (EndPoint)T.RemoteEndPoint; //Server的EndPoint
+            byte[] B = new byte[1023]; //接收用的byte陣列
+            int inLen = 0; //接收的位元組數目
+            string Msg; //接收到的完整訊息
+            string St; //命令碼
+            string Str; //訊息內容(不含命令碼)
+            while(true)
+            {
+                try
+                {
+                    inLen = T.ReceiveFrom(B, ref ServerEP); //收聽資訊並取得位元組數
+                }
+                catch (Exception)
+                {
+                    T.Close(); //關閉通訊器
+                    listBox_User.Items.Clear(); //清除線上名單
+                    MessageBox.Show("伺服器斷線了!");
+                    button_Login.Enabled = true; //連線按鍵恢復可用
+                    button_Enter.Enabled = false; //要連線後才可發送
+                    Th.Abort(); //刪除執行緒
+                }
+
+                Msg = Encoding.Default.GetString(B, 0, inLen); //解讀完整訊息
+                St = Msg.Substring(0, 1); //取命令碼(第1個字)
+                Str = Msg.Substring(1); //取命令碼後的訊息
+
+                switch(St)
+                {
+                    case "L":
+                        listBox_User.Items.Clear(); //清除線上名單
+                        string[] M = Str.Split(','); //拆解名單成陣列
+                        for(int i = 0; i < M.Length; i++)
+                        {
+                            listBox_User.Items.Add(M[i]); //逐一加入名單
+                        }
+                        break;
+
+                    case "1": //接收廣播訊息
+                        textBox_Messages.Text += "(公開)" + Str + "\r\n"; //顯示訊息並換行
+                        textBox_Messages.SelectionStart = textBox_Messages.Text.Length; //游標移到最後
+                        textBox_Messages.ScrollToCaret(); //捲動到游標位置
+                        break;
+
+                    case "2":
+                        textBox_Messages.Text += "(私密)" + Str + "\r\n"; //顯示私密訊息並換行
+                        textBox_Messages.SelectionStart = textBox_Messages.Text.Length; //游標移到最後
+                        textBox_Messages.ScrollToCaret(); //捲動到游標位置
+                        break;
+                }
+            }
+        }
+    }
+}
