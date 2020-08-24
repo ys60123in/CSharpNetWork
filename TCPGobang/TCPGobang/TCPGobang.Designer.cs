a998728dba843c96a53ed6110fa3fc435d66547e
ys60123in <ys60123in@gmail.com>
2020-08-24 16:33:55 +0800

    新增TCP 線上五子棋程式

---
+++ TCPGobang/TCPGobang/TCPGobang.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace TCPGobang
+{
+    partial class TCPGobang
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_Board = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_ServerIP = new System.Windows.Forms.TextBox();
+            this.textBox_ServerPort = new System.Windows.Forms.TextBox();
+            this.textBox_UserName = new System.Windows.Forms.TextBox();
+            this.button_Login = new System.Windows.Forms.Button();
+            this.listBox_OnlineUser = new System.Windows.Forms.ListBox();
+            this.textBox_SysMsg = new System.Windows.Forms.TextBox();
+            this.button_Reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel_Board
+            // 
+            this.panel_Board.BackColor = System.Drawing.Color.White;
+            this.panel_Board.Location = new System.Drawing.Point(0, 0);
+            this.panel_Board.Name = "panel_Board";
+            this.panel_Board.Size = new System.Drawing.Size(570, 570);
+            this.panel_Board.TabIndex = 0;
+            this.panel_Board.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_Board_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(576, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "伺服器IP：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(576, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "伺服器Port：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(576, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "玩家名稱：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(576, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "線上使用者";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(576, 498);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "系統訊息";
+            // 
+            // textBox_ServerIP
+            // 
+            this.textBox_ServerIP.Location = new System.Drawing.Point(578, 24);
+            this.textBox_ServerIP.Name = "textBox_ServerIP";
+            this.textBox_ServerIP.Size = new System.Drawing.Size(120, 22);
+            this.textBox_ServerIP.TabIndex = 6;
+            // 
+            // textBox_ServerPort
+            // 
+            this.textBox_ServerPort.Location = new System.Drawing.Point(578, 73);
+            this.textBox_ServerPort.Name = "textBox_ServerPort";
+            this.textBox_ServerPort.Size = new System.Drawing.Size(120, 22);
+            this.textBox_ServerPort.TabIndex = 7;
+            // 
+            // textBox_UserName
+            // 
+            this.textBox_UserName.Location = new System.Drawing.Point(578, 141);
+            this.textBox_UserName.Name = "textBox_UserName";
+            this.textBox_UserName.Size = new System.Drawing.Size(120, 22);
+            this.textBox_UserName.TabIndex = 8;
+            // 
+            // button_Login
+            // 
+            this.button_Login.Location = new System.Drawing.Point(578, 169);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(120, 23);
+            this.button_Login.TabIndex = 9;
+            this.button_Login.Text = "登入伺服器";
+            this.button_Login.UseVisualStyleBackColor = true;
+            this.button_Login.Click += new System.EventHandler(this.button_Login_Click);
+            // 
+            // listBox_OnlineUser
+            // 
+            this.listBox_OnlineUser.FormattingEnabled = true;
+            this.listBox_OnlineUser.ItemHeight = 12;
+            this.listBox_OnlineUser.Location = new System.Drawing.Point(578, 216);
+            this.listBox_OnlineUser.Name = "listBox_OnlineUser";
+            this.listBox_OnlineUser.Size = new System.Drawing.Size(120, 268);
+            this.listBox_OnlineUser.TabIndex = 10;
+            // 
+            // textBox_SysMsg
+            // 
+            this.textBox_SysMsg.Location = new System.Drawing.Point(578, 513);
+            this.textBox_SysMsg.Name = "textBox_SysMsg";
+            this.textBox_SysMsg.Size = new System.Drawing.Size(120, 22);
+            this.textBox_SysMsg.TabIndex = 11;
+            // 
+            // button_Reset
+            // 
+            this.button_Reset.Location = new System.Drawing.Point(578, 543);
+            this.button_Reset.Name = "button_Reset";
+            this.button_Reset.Size = new System.Drawing.Size(120, 23);
+            this.button_Reset.TabIndex = 12;
+            this.button_Reset.Text = "清除重玩";
+            this.button_Reset.UseVisualStyleBackColor = true;
+            this.button_Reset.Click += new System.EventHandler(this.button_Reset_Click);
+            // 
+            // TCPGobang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 571);
+            this.Controls.Add(this.button_Reset);
+            this.Controls.Add(this.textBox_SysMsg);
+            this.Controls.Add(this.listBox_OnlineUser);
+            this.Controls.Add(this.button_Login);
+            this.Controls.Add(this.textBox_UserName);
+            this.Controls.Add(this.textBox_ServerPort);
+            this.Controls.Add(this.textBox_ServerIP);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel_Board);
+            this.Name = "TCPGobang";
+            this.Text = "線上五子棋";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TCPGobang_FormClosing);
+            this.Load += new System.EventHandler(this.TCPGobang_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_Board;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_ServerIP;
+        private System.Windows.Forms.TextBox textBox_ServerPort;
+        private System.Windows.Forms.TextBox textBox_UserName;
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.ListBox listBox_OnlineUser;
+        private System.Windows.Forms.TextBox textBox_SysMsg;
+        private System.Windows.Forms.Button button_Reset;
+    }
+}
+
---
+++ TCPGobang/TCPGobang/TCPGobang.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.PowerPacks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCPGobang
+{
+    public partial class TCPGobang : Form
+    {
+        ShapeContainer CVS; //畫布物件(棋盤)
+        byte[,] S; //對應棋盤狀態的陣列:0為空格，1為黑子，2為白子
+        Socket T; //通訊物件
+        Thread Th; //網路監聽執行緒
+        string User; //使用者
+
+        public TCPGobang()
+        {
+            InitializeComponent();
+        }
+
+        //繪製棋盤與加入畫布
+        private void TCPGobang_Load(object sender, EventArgs e)
+        {
+            Bitmap bg = new Bitmap(570, 570); //棋盤影像物件
+            Graphics g = Graphics.FromImage(bg); //棋盤影像繪圖物件
+            g.Clear(Color.White); //設白色為背景色
+            for (int i = 0; i < 19; i++)
+            {
+                g.DrawLine(Pens.Black, i * 30 + 15, 15, i * 30 + 15, 30 * 18 + 15); //畫19條垂直線
+            }
+            for (int j = 0; j < 19; j++)
+            {
+                g.DrawLine(Pens.Black, 15, j * 30 + 15, 30 * 18 + 15, j * 30 + 15); //畫19條水平線
+            }
+            panel_Board.BackgroundImage = bg; //貼上棋盤影像為panel背景
+            CVS = new ShapeContainer(); //宣告畫布物件
+            panel_Board.Controls.Add(CVS); //畫布物件加入panel_Board
+            S = new byte[19, 19]; //宣告棋盤資訊陣列
+        }
+
+        //關閉視窗離線
+        private void TCPGobang_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (button_Login.Enabled == false)
+            {
+                Send("9" + User); //傳送自己離線的訊息給伺服器
+                T.Close(); //關閉網路通訊器T
+            }
+        }
+
+        //登入
+        private void button_Login_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false; //忽略跨執行緒錯誤
+            string IP = textBox_ServerIP.Text; //伺服器IP
+            int Port = int.Parse(textBox_ServerPort.Text); //伺服器Port
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(IP), Port); //伺服器的連線端點資訊
+
+            //建立可以雙向通訊的TCP連線
+            T = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            User = textBox_UserName.Text; //使用者名稱
+
+            try
+            {
+                T.Connect(EP); //連上伺服器的端點EP(類似撥號給電話總機)
+                Th = new Thread(Listen); //建立監聽執行緒
+                Th.IsBackground = true; //設為背景執行緒
+                Th.Start(); //開始監聽
+                textBox_SysMsg.Text = "已連線伺服器! " + "\r\n";
+                Send("0" + User);
+            }
+            catch (Exception)
+            {
+                textBox_SysMsg.Text = "無法連上伺服器! " + "\r\n"; //連線失敗時顯示訊息
+                return;
+            }
+            button_Login.Enabled = false; //讓連線按鍵失敗，避免重複連線
+        }
+
+        //重玩
+        private void button_Reset_Click(object sender, EventArgs e)
+        {
+            if (listBox_OnlineUser.SelectedIndex >= 0) //如果有對手時
+            {
+                Send("5" + "C" + "|" + listBox_OnlineUser.SelectedItem);
+                panel_Board.Enabled = false; //輪到對手，你不能繼續下棋
+            }
+            CVS.Shapes.Clear(); //清除棋子
+            S = new byte[19, 19]; //清除棋盤資訊
+            panel_Board.Enabled = true; //開放任一方下棋
+        }
+
+        //下棋的動作
+        private void panel_Board_MouseDown(object sender, MouseEventArgs e)
+        {
+            int i = e.X / 30; //算出是第幾個水平向的棋格
+            int j = e.Y / 30; //算出是第幾個垂直向的棋格
+            if(S[i,j] == 0)
+            {
+                Chess(i, j, Color.Black); //畫黑子
+                S[i, j] = 1; //記為黑子
+                if (listBox_OnlineUser.SelectedIndex >= 0) //如果有對手時
+                {
+                    Send("6" + i.ToString() + "," + j.ToString() + "|" + listBox_OnlineUser.SelectedItem);
+                    panel_Board.Enabled = false; //輪到對手，你不能繼續下棋
+                }
+                if (chk5(i, j, 1))
+                {
+                    MessageBox.Show("你贏了! ");
+                    panel_Board.Enabled = false; //結束後設為必須重設才可下棋
+                }
+            }
+        }
+
+        //畫棋子的副程序
+        private void Chess(int i, int j, Color BW)
+        {
+            OvalShape C = new OvalShape(); //建立一個圓形的Shape物件
+            C.Width = 26; //寬
+            C.Height = 26; //高
+            C.Left = i * 30 + 2; //左邊座標
+            C.Top = j * 30 + 2; //頂部座標
+            C.FillStyle = FillStyle.Solid; //實心填滿
+            C.FillColor = BW; //填黑或白色
+            C.Parent = CVS; //將圓形Shape物件加入畫布(棋盤)
+        }
+
+        //檢查是否五連線的程式
+        private bool chk5(int i, int j, byte tg)
+        {
+            //水平連線檢查
+            int n = 0;
+            for (int m = i - 4; m <= i + 4; m++)
+            {
+                n = nn(m, j, tg, n); //檢視下一格
+                if (n == 5) return true;
+            }
+            //垂直連線檢查
+            n = 0;
+            for (int m = j - 4; m <= j + 4; m++)
+            {
+                n = nn(i, m, tg, n); //檢視下一格
+                if (n == 5) return true;
+            }
+            //左上到右下檢查
+            n = 0;
+            for (int m = -4; m <= 4; m++)
+            {
+                n = nn(i + m, j + m, tg, n); //檢視下一格
+                if (n == 5) return true;
+            }
+            //右上到左下檢查
+            n = 0;
+            for (int m = -4; m <= 4; m++)
+            {
+                n = nn(i - m, j + m, tg, n); //檢視下一格
+                if (n == 5) return true;
+            }
+
+            return false;
+        }
+        //搭配檢查用(計算連線)
+        private int nn(int i, int j, byte tg, int n)
+        {
+            if (i < 0 || i > 18 || j < 0 || j > 18) return n;
+            if (S[i, j] == tg)
+            {
+                return n + 1; //累計數值+1
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //傳送訊息給Server
+        private void Send(string Str)
+        {
+            byte[] B = Encoding.Default.GetBytes(Str);
+            T.Send(B, 0, B.Length, SocketFlags.None); //使用連線物件傳送資料
+        }
+
+        //監聽Server信息
+        private void Listen()
+        {
+            EndPoint ServerEP = (EndPoint)T.RemoteEndPoint; //Server的EndPoint
+            byte[] B = new byte[1023]; //接收用的byte陣列
+            int inLen = 0; //接收的位元組數目
+            string Msg; //接收到的完整訊息
+            string St; //命令碼
+            string Str; //訊息內容(不含命令碼)
+            while (true)
+            {
+                try
+                {
+                    inLen = T.ReceiveFrom(B, ref ServerEP); //收聽資訊並取得位元組數
+                }
+                catch (Exception)
+                {
+                    T.Close(); //關閉通訊器
+                    listBox_OnlineUser.Items.Clear(); //清除線上名單
+                    MessageBox.Show("伺服器斷線了!");
+                    button_Login.Enabled = true; //連線按鍵恢復可用
+                    Th.Abort(); //刪除執行緒
+                }
+
+                Msg = Encoding.Default.GetString(B, 0, inLen); //解讀完整訊息
+                St = Msg.Substring(0, 1); //取命令碼(第1個字)
+                Str = Msg.Substring(1); //取命令碼後的訊息
+
+                switch (St)
+                {
+                    case "L":
+                        listBox_OnlineUser.Items.Clear(); //清除線上名單
+                        string[] M = Str.Split(','); //拆解名單成陣列
+                        for (int i = 0; i < M.Length; i++)
+                        {
+                            listBox_OnlineUser.Items.Add(M[i]); //逐一加入名單
+                        }
+                        break;
+
+                    case "5": //清除棋盤訊息
+                        CVS.Shapes.Clear(); //清除棋子
+                        S = new byte[19, 19]; //清除棋盤資訊
+                        panel_Board.Enabled = true; //開放任一方下棋
+                        break;
+
+                    case "6": //對手下棋訊息
+                        string[] D = Str.Split(','); //切割座標訊息
+                        int x = int.Parse(D[0]); //水平向的棋格
+                        int y = int.Parse(D[1]); //垂直向的棋格
+                        Chess(x, y, Color.White); //畫白子
+                        S[x, y] = 2; //記為白子
+                        panel_Board.Enabled = true; //對手下好了，換你下棋
+                        if (chk5(x, y, 2))
+                        {
+                            MessageBox.Show("你輸了! "); //檢查對手是否五連線了
+                            panel_Board.Enabled = false; //結束後設為必須重設才可下棋
+                        }
+                        break;
+                }
+            }
+        }
+
+        
+    }
+}
